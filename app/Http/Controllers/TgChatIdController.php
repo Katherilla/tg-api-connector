d99e4a7d5c947d5587bf223a95b5d600fbdd88e0
Katherilla <katyakatanaeva@mail.ru>
2024-05-05 00:06:41 +0300

    fix sending message to telegram

--- app/Http/Controllers/TgChatIdController.php
+++ app/Http/Controllers/TgChatIdController.php
@@ -12,7 +12,7 @@ class TgChatIdController extends Controller
         $results = json_decode(file_get_contents(config('url_config.DATA_AGGREGATOR_URL') . '/api/daily_report'));
         $yesterday= \Carbon\Carbon::yesterday()->toDateString();
         foreach($results as $res){
-            $msg = 'Stats for ' . $yesterday;
+            $msg = "Stats for " . $yesterday;
             $msg .= "\nGroup name: " . $res->group_name;
             $msg .= "\nPosts: " . $res->cnt_posts;
             $msg .= "\nLikes: " . $res->cnt_likes;
@@ -25,6 +25,6 @@ class TgChatIdController extends Controller
     }
 
     public static function tg_send_message($group_chat_id, $msg) : void{
-        file_get_contents('http://api.telegram.org/' . config('tg_config.TG_TOKEN') . '/sendMessage&chat_id=' . $group_chat_id . '&text=' . $msg);
+        file_get_contents('http://api.telegram.org/bot' . config('tg_config.TG_TOKEN') . '/sendMessage?chat_id=' . $group_chat_id . '&text=' . urlencode($msg));
     }
 }
